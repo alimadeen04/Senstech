54058a6ede211a90d2ce8364a39c5b6cf0688173
Vinesha Shaik <vinneesha@gmail.com>
2025-07-18 15:16:34 -0400

    font fixed

--- CreatConnect/main.py
+++ CreatConnect/main.py
@@ -10,15 +10,23 @@ from menu_screen import MenuScreen # Ensure this file and class exist
 from user_interface import CreatConnectUI # The main sensor UI
 from visual import VisualScreen # Ensure this file and class exist
 
+import os
+
 PAPER_COLOR = (0.95, 0.95, 0.9, 1) # Slightly off-white for paper
 Window.clearcolor = PAPER_COLOR
-LabelBase.register(name="Roboto", fn_regular="ArchitectsDaughter.ttf")
-    
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# *** THIS LINE IS THE FIX ***
+# Go up two directories ('..', '..'), then into the 'Roboto' folder
+roboto_font_path = os.path.join(current_dir, '..', 'Roboto', 'static', 'Roboto-Regular.ttf')
+
+LabelBase.register(name="Roboto", fn_regular=roboto_font_path)
+
+
 class CreatConnectApp(App):
     def build(self):
         # Initialize the list to store all creatinine readings as an instance attribute.
         self.all_creatinine_readings = []
-
 
         # Create the screen manager
         sm = ScreenManager()
@@ -37,7 +45,6 @@ class CreatConnectApp(App):
         visual_screen = Screen(name='visual_screen')
         visual_screen.add_widget(VisualScreen()) # VisualScreen should be defined in visual.py
         sm.add_widget(visual_screen)
-
 
         # Set the initial screen to the menu
         sm.current = 'menu_screen'
